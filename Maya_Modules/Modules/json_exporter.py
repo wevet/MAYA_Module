68b19fb655343f53fa3d2e06b0ba04b19076be0b
Nagasawa <waltzfor.floword@gmail.com>
2022-05-31 20:06:40 +0900

    modify json format

--- Maya_Modules/Modules/json_exporter.py
+++ Maya_Modules/Modules/json_exporter.py
@@ -11,7 +11,7 @@ usage
 import json_exporter
 import importlib
 importlib.reload(json_exporter)
-json_exporter.save_selected_joints_to_json(1, 4, False)
+json_exporter.save_selected_joints_to_json(1, 10, False)
 """
 
 def save_selected_joints_to_json(start_time, end_time, export_joint: bool):
@@ -25,28 +25,64 @@ def save_selected_joints_to_json(start_time, end_time, export_joint: bool):
         objects = cmds.ls(selection=True, type="joint", dag=True, long=True)
     else:
         objects = cmds.ls(selection=True, type="transform", dag=True, long=True)
-    joints = {}
+    joints = []
     time_range = range(start_time, end_time+1, 1)
 
-    index = 0
+    #joints['start'] = start_time
+    #joints['end'] = end_time
+    prop_name = ['rx', 'ry', 'rz', 'tx', 'ty', 'tz']
+
+    for obj in objects:
+        j = 0
+        for prop in prop_name:
+            joints.append(j)
+            joint_dict = _get_all_frame_key(start_time, end_time, obj, prop)
+            joints[j] = joint_dict
+            j += 1
+
+    """
     for obj in objects:
         joint_dict = {}
         #short_name = obj.split("|")[-1]
         short_name = 'bone_' + str(index)
-        index += 1
         is_root = False
         if "_ROOT_" in short_name:
             is_root = True
-
         for frame in time_range:
-            if is_root:
-                joint_dict[frame] = _get_frame_dist_for_root(obj, frame)
-            else:
-                joint_dict[frame] = _get_frame_dict(obj, frame)
-        joints['start'] = start_time
-        joints['end'] = end_time
-        joints[short_name] = joint_dict
+            joint_dict[frame] = _get_frame_dict(obj, frame)
+        joints[index] = joint_dict
+    """
     _export_json(joints, scene_name)
+
+def _get_all_frame_key(start_time, end_time, obj, prop):
+    time_range = range(start_time, end_time+1, 1)
+    frame_dict = {
+        'Name': prop
+    }
+    for frame in time_range:
+        rot_order = cmds.getAttr('%s.rotateOrder' % obj)
+        joint_mat = om.MMatrix()
+        om.MScriptUtil.createMatrixFromList(cmds.getAttr('%s.matrix' % obj, time=frame), joint_mat)
+        joint_transform_mat = om.MTransformationMatrix(joint_mat)
+        euler_rot = joint_transform_mat.eulerRotation()
+        euler_rot.reorderIt(rot_order)
+        translation = joint_transform_mat.getTranslation(om.MSpace.kTransform)
+        angles = [math.degrees(angle) for angle in (euler_rot.x, euler_rot.y, euler_rot.z)]
+
+        if prop is 'rx':
+            frame_dict[str(frame)] = (angles[0])
+        if prop is 'ry':
+            frame_dict[str(frame)] = (angles[1])
+        if prop is 'rz':
+            frame_dict[str(frame)] = (angles[2])
+        if prop is 'tx':
+            frame_dict[str(frame)] = "%.8f" % translation.x
+        if prop is 'ty':
+            frame_dict[str(frame)] = "%.8f" % translation.y
+        if prop is 'tz':
+            frame_dict[str(frame)] = "%.8f" % translation.z
+    return frame_dict
+
 
 #non root joints should pass values from object space
 def _get_frame_dict(obj, frame):
